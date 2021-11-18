0ffb0abe368bc3192aaba7ec64f889f364f5ffdc
nipo1019 <nikita.popov@netcracker.com>
2021-11-18 22:30:20 +0400

    [lab-5] update

--- lab-5/src/java/com/johnpier/lab5/CalculatorApp.java
+++ lab-5/src/java/com/johnpier/lab5/CalculatorApp.java
@@ -11,7 +11,7 @@ public class CalculatorApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(CalculatorApp.class.getResource("main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Calculator");
         stage.setScene(scene);
         stage.show();
