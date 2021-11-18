97e111f6a3680770c6915f7c79b0eb28e1931abc
nipo1019 <nikita.popov@netcracker.com>
2021-11-18 22:06:06 +0400

    [lab-5] init

---
+++ lab-5/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.johnpier.lab {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+//    requires eu.hansolo.tilesfx;
+
+    opens com.johnpier.lab5 to javafx.fxml;
+    exports com.johnpier.lab5;
+}
