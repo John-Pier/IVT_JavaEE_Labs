7a5b08b6165d4c9568a744f59a01711b60057f72
nipo1019 <nikita.popov@netcracker.com>
2021-12-05 01:10:10 +0400

    [lab-7] update

--- lab-7/src/Main.java
+++ lab-7/src/Main.java
@@ -4,7 +4,6 @@ import javax.xml.parsers.*;
 import javax.xml.transform.*;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
-import java.io.*;
 
 public class Main {
 
@@ -12,23 +11,22 @@ public class Main {
     private static Transformer transformer;
 
     static {
-        var factory = DocumentBuilderFactory.newInstance();
-        var transformerFactory =  TransformerFactory.newInstance();
         try {
-            builder  = factory.newDocumentBuilder();
-            transformer = transformerFactory.newTransformer();
-            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            builder  = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            transformer = TransformerFactory.newInstance().newTransformer();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     public static void main(String[] args) {
-        if(builder == null) {
+        if (builder == null || transformer == null) {
+            System.err.println("Error, some instances is null!");
             return;
         }
         try {
             var document = builder.parse(args[0]);
+            transformer.setOutputProperty(OutputKeys.DOCTYPE_SYSTEM, document.getDoctype().getSystemId());
 
             var students = document.getElementsByTagName("student");
             var studentSubjects = ((Element)students.item(0)).getElementsByTagName("subject");
@@ -41,6 +39,8 @@ public class Main {
                 studentAverageNode.setTextContent(String.valueOf(correctAverage));
             }
 
+            System.out.println("Correct Average: " + correctAverage + "\nCurrent Average: " + currentAverage);
+
             createDocumentWithAverage(students, args[1]);
         } catch (Exception e) {
             e.printStackTrace();
@@ -50,22 +50,23 @@ public class Main {
     private static double getCorrectAverage(NodeList nodeList) {
         double markSum = 0;
         for (int i = 0; i < nodeList.getLength(); i++) {
-            markSum += Integer.parseInt(((Element)nodeList.item(i)).getAttribute("mark")); // todo: checks
+            markSum += Integer.parseInt(((Element)nodeList.item(i)).getAttribute("mark"));
         }
         return markSum / nodeList.getLength();
     }
 
-    private static void createDocumentWithAverage(NodeList nodeList, String fileNamePath) throws IOException, TransformerException {
+    private static void createDocumentWithAverage(NodeList nodeList, String fileNamePath) throws TransformerException {
         if (builder == null || transformer == null) {
+            System.err.println("Error, some instances is null!");
             return;
         }
 
         var correctDocument = builder.newDocument();
-        var source = new DOMSource();
-        var streamResult = new StreamResult(new FileWriter(fileNamePath)); // todo: ref
+        var source = new DOMSource(correctDocument);
+        var streamResult = new StreamResult(fileNamePath);
 
         for(int i = 0; i < nodeList.getLength(); i++) {
-            correctDocument.importNode(nodeList.item(i), true);
+            correctDocument.appendChild(correctDocument.importNode(nodeList.item(i), true));
         }
 
         transformer.transform(source, streamResult);
