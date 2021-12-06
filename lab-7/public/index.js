8ade6043511c694c3822d7758e583d9812ef3243
nipo1019 <nikita.popov@netcracker.com>
2021-12-06 10:43:40 +0400

    [lab-7] update

--- lab-7/public/index.js
+++ lab-7/public/index.js
@@ -1,3 +1,27 @@
-console.log("works");
+'use strict';
 
-let t = RegExp("(-+)?(0-9)+(,.)+(0-9)+")
+const formName = "calculationForm";
+
+const calculationFormFieldNames = {
+    FIRST: "first",
+    SECOND: "second",
+    OPERATION: "operation",
+};
+
+window.onload = () => makeSubmitHandler();
+
+function makeSubmitHandler() {
+    const form = document.forms.namedItem(formName);
+    if(!form) {
+        return;
+    }
+
+    form.addEventListener("submit", e => {
+        const operation = form.elements[calculationFormFieldNames.OPERATION].value;
+        const secondOperand =  form.elements[calculationFormFieldNames.SECOND].value;
+        if(operation === "/" && secondOperand === 0) {
+            e.preventDefault();
+            alert("The second operand must not be 0 if division is selected.!");
+        }
+    })
+}
---
+++ lab-7/public/result.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const operationsMap = {
+    '-': (a,b) => a - b,
+    '+': (a,b) => a + b,
+    '/': (a,b) => a / b,
+    '*': (a,b) => a * b,
+};
+
+const queriesParamsNames = {
+    FIRST: "first",
+    SECOND: "second",
+    OPERATION: "operation",
+};
+
+const tableBodyId = 'tbody-result';
+
+window.onload = () => setCalculationResult();
+
+function setCalculationResult() {
+    const tableBody = document.getElementById(tableBodyId);
+    const queriesParams =  new URL(window.location.href).searchParams;
+    const first = queriesParams.get(queriesParamsNames.FIRST);
+    const second = queriesParams.get(queriesParamsNames.SECOND);
+    const operation = queriesParams.get(queriesParamsNames.OPERATION);
+
+    console.log([first, second, operation]);
+
+    if(![first, second, operation].filter(Boolean).length) {
+        alert("Calculation failed!");
+        return;
+    }
+
+    const row = document.createElement("tr");
+    row.innerHTML = `<td>${operation}</td><td>${first}</td><td>${second}</td><td>${operationsMap[operation](first, second)}</td>`;
+    tableBody.append(row);
+}
