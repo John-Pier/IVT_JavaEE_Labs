682506a865dae9b2fc3a75b918a5c2b2f4ccaabf
nipo1019 <nikita.popov@netcracker.com>
2021-12-09 00:32:59 +0400

    [lab-6] update

--- lab-6/src/main/java/Main.java
+++ lab-6/src/main/java/Main.java
@@ -13,14 +13,42 @@ public class Main {
 
         try(var connection = DriverManager.getConnection(CONNECTION_URL, props)) {
             var statement = connection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
-            var resultSet = statement.executeQuery("SELECT * FROM ARTIST");
+            // var resultSet = statement.executeQuery("SELECT * FROM ARTIST");
 
-            printArtistTable(resultSet);
+            testOperations(connection);
 
             System.out.println("\nОсуществляется выход...");
+            statement.close();
         } catch (Exception ex) {
             ex.printStackTrace();
         }
+    }
+
+    private static void testOperations(Connection connection) throws SQLException {
+        var artistName = "updateMe";
+        var artistNextName = "deleteMe";
+
+        var statement = connection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+
+        var preparedInsertStatement = connection.prepareStatement("INSERT INTO  ARTIST(NAME) VALUES (?)");
+        var preparedUpdateStatement = connection.prepareStatement("UPDATE ARTIST SET  NAME = ? WHERE NAME = ?");
+        var preparedDeleteStatement = connection.prepareStatement("DELETE  FROM  ARTIST WHERE NAME = ?");
+
+        System.out.println("\nДобавление " + artistName);
+        preparedInsertStatement.setString(1, artistName);
+        preparedInsertStatement.executeUpdate();
+        printArtistTable(statement.executeQuery("SELECT * FROM ARTIST"));
+
+        System.out.println("\nОбновление " + artistName + "->" + artistNextName);
+        preparedUpdateStatement.setString(1, artistNextName);
+        preparedUpdateStatement.setString(2, artistName);
+        preparedUpdateStatement.executeUpdate();
+        printArtistTable(statement.executeQuery("SELECT * FROM ARTIST"));
+
+        System.out.println("\nУдаление " + artistNextName);
+        preparedDeleteStatement.setString(1, artistNextName);
+        preparedDeleteStatement.executeUpdate();
+        printArtistTable(statement.executeQuery("SELECT * FROM ARTIST"));
     }
 
     private static void printArtistTable(ResultSet resultSet) {
