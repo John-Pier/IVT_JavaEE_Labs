92c6694fa7e9647e5d11d09c46a5d93dd2be8c1e
John Pier <warhamer10tw@gmail.com>
2022-03-23 23:06:01 +0400

    lab-postgres-ql upd

--- lab-postgre-sql/src/Main.java
+++ lab-postgre-sql/src/Main.java
@@ -4,6 +4,9 @@ import java.sql.*;
 import java.util.Properties;
 
 public class Main {
+
+    public static DBHelper dbHelper;
+
     public static void main(String[] args) {
         if(args.length == 0) {
             return;
@@ -13,9 +16,12 @@ public class Main {
         props.setProperty("user","postgres");
         props.setProperty("password","299792458");
         props.setProperty("ssl","false");
+//        var statement = connection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
 
         try(var connection = DriverManager.getConnection(args[0], props)) {
             var statement = connection.createStatement();
+
+            dbHelper = new DBHelper(connection);
 
             var resultSet = statement.executeQuery(
                     """
@@ -36,52 +42,139 @@ public class Main {
 
     private static void insertOperations(Connection connection) throws SQLException {
         var projectName = "Generated Pr " + Math.exp(Math.random());
-        var description = "Generated Description " + Math.exp(Math.random());
-
-        var statement = connection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
-
-        var projectsInsert = connection.prepareStatement("INSERT INTO  projects_systems.public.\"Projects\" (description) VALUES (?)");
-        var teamInsert = connection.prepareStatement("INSERT INTO  projects_systems.public.\"Team\" (name, description) VALUES (?, ?)");
-        var teamProjectsInsert = connection.prepareStatement("INSERT INTO  projects_systems.public.\"Team_Projects\" (project_id, team_id) VALUES (?, ?)");
-        var employeeProjectsInsert = connection.prepareStatement("INSERT INTO  projects_systems.public.\"Employee\" (name, start_date, position_id, data) VALUES (?, '2001.12.11', ?, 'No data')");
-        var categoriesInsert = connection.prepareStatement("INSERT INTO  projects_systems.public.\"P_Categories\" (name, description) VALUES (?, ?)");
-        var userInsert = connection.prepareStatement("INSERT INTO  projects_systems.public.\"User\" (name, employee_id, credentials, data) VALUES (?, ?, ?, ?)");
-        var employeeTeamInsert = connection.prepareStatement("INSERT INTO  projects_systems.public.\"Employee_Team\" (employee_id, team_id, role_id, start_date) VALUES (?, ?, ?, ?)");
-        var categoriesProjectsInsert = connection.prepareStatement("INSERT INTO  projects_systems.public.\"P_Categories_Projects\" (project_id, p_category_id) VALUES (?, ?)");
-        var employeeSkillInsert = connection.prepareStatement("INSERT INTO  projects_systems.public.\"Employee_Skill\" (employee_id, skill_id, level) VALUES (?, ?, ?)");
-        var skillInsert = connection.prepareStatement("INSERT INTO  projects_systems.public.\"Skill\" (name, description) VALUES (?, ?)");
-        var historyInsert = connection.prepareStatement("INSERT INTO  projects_systems.public.\"E_History\" (employee_id, description, change_date) VALUES (?, ?, ?)");
-        var positionInsert = connection.prepareStatement("INSERT INTO  projects_systems.public.\"Position\" (name, description) VALUES (?, ?)");
-        var roleInsert = connection.prepareStatement("INSERT INTO  projects_systems.public.\"Role\" (name, description) VALUES (?, ?)");
-        var ruleInsert = connection.prepareStatement("INSERT INTO  projects_systems.public.\"Rule\" (name, description) VALUES (?, ?)");
-        var repositoryInsert = connection.prepareStatement("INSERT INTO  projects_systems.public.\"Repository\" (link, description) VALUES (?, ?)");
-        var repositoryProjectsInsert = connection.prepareStatement("INSERT INTO  projects_systems.public.\"Repository_Projects\" (project_id, repository_id) VALUES (?, ?)");
-        var ruleProjectsInsert = connection.prepareStatement("INSERT INTO  projects_systems.public.\"Rule_Projects\" (rule_id, project_id) VALUES (?, ?)");
-
-
+        var projectsInsert = connection.prepareStatement(
+                "INSERT INTO  projects_systems.public.\"Projects\" (description) VALUES (?)"
+        );
         projectsInsert.setString(1, projectName);
         projectsInsert.executeQuery();
+        var projectId = dbHelper.selectProjectId(projectName);
 
-        teamInsert.setString(1, DBHelper.generateName());
+        var teamInsert = connection.prepareStatement(
+                "INSERT INTO  projects_systems.public.\"Team\" (name, description) VALUES (?, ?)"
+        );
+        var name = DBHelper.generateName();
+        teamInsert.setString(1, name);
         teamInsert.setString(2, DBHelper.generateDescription());
         teamInsert.executeQuery();
+        var teamId = dbHelper.selectTeamId(name);
+
+        var teamProjectsInsert = connection.prepareStatement(
+                "INSERT INTO  projects_systems.public.\"Team_Projects\" (project_id, team_id) VALUES (?, ?)"
+        );
+        teamProjectsInsert.setInt(1, projectId);
+        teamProjectsInsert.setInt(2, teamId);
+        teamProjectsInsert.executeQuery();
+
+        var positionName =  DBHelper.generateName();
+        var positionInsert = connection.prepareStatement(
+                "INSERT INTO  projects_systems.public.\"Position\" (name, description) VALUES (?, ?)"
+        );
+        positionInsert.setString(1, positionName);
+        positionInsert.setString(2, DBHelper.generateDescription());
+        var positionId =  dbHelper.selectPositionId(positionName);
+
+        var employeeName =  DBHelper.generateName();
+        var employeeInsert = connection.prepareStatement(
+                "INSERT INTO  projects_systems.public.\"Employee\" (name, start_date, position_id, data) VALUES (?, '2001.12.11', ?, 'No data')"
+        );
+        employeeInsert.setString(1, employeeName);
+        employeeInsert.setInt(2, positionId);
+        employeeInsert.executeQuery();
+        var employeeId = dbHelper.selectEmployeeId(employeeName);
+
+        var userInsert = connection.prepareStatement(
+                "INSERT INTO  projects_systems.public.\"User\" (name, employee_id, credentials, data) VALUES (?, ?, '-?-', 'No data')"
+        );
+        userInsert.setString(1, DBHelper.generateName());
+        userInsert.setInt(2, employeeId);
+        userInsert.executeQuery();
+
+        var categoriesName = DBHelper.generateName();
+        var categoriesInsert = connection.prepareStatement(
+                "INSERT INTO  projects_systems.public.\"P_Categories\" (name, description) VALUES (?, ?)"
+        );
+        categoriesInsert.setString(1, categoriesName);
+        categoriesInsert.setString(2, DBHelper.generateDescription());
+        categoriesInsert.executeQuery();
+        var categoriesId = dbHelper.selectCategoriesId(categoriesName);
+
+        var categoriesProjectsInsert = connection.prepareStatement(
+                "INSERT INTO  projects_systems.public.\"P_Categories_Projects\" (project_id, p_category_id) VALUES (?, ?)"
+        );
+        categoriesProjectsInsert.setInt(1, projectId);
+        categoriesProjectsInsert.setInt(2, categoriesId);
+        categoriesProjectsInsert.executeQuery();
+
+        var repositoryDescription = DBHelper.generateDescription();
+        var repositoryInsert = connection.prepareStatement(
+                "INSERT INTO  projects_systems.public.\"Repository\" (link, description) VALUES ('-NO LINK-', ?)"
+        );
+        repositoryInsert.setString(1, repositoryDescription);
+        var repositoryId = dbHelper.selectRepositoryIdByDescription(repositoryDescription);
+
+        var repositoryProjectsInsert = connection.prepareStatement(
+                "INSERT INTO  projects_systems.public.\"Repository_Projects\" (project_id, repository_id) VALUES (?, ?)"
+        );
+        repositoryProjectsInsert.setInt(1, projectId);
+        repositoryProjectsInsert.setInt(2, repositoryId);
+        repositoryProjectsInsert.executeQuery();
+
+        var ruleName = DBHelper.generateName();
+        var ruleInsert = connection.prepareStatement(
+                "INSERT INTO  projects_systems.public.\"Rule\" (name, description) VALUES (?, ?)"
+        );
+        ruleInsert.setString(1, ruleName);
+        ruleInsert.setString(1, DBHelper.generateDescription());
+        ruleInsert.executeQuery();
+        var ruleId = dbHelper.selectRuleId(ruleName);
+
+        var ruleProjectsInsert = connection.prepareStatement(
+                "INSERT INTO  projects_systems.public.\"Rule_Projects\" (rule_id, project_id) VALUES (?, ?)"
+        );
+        ruleProjectsInsert.setInt(1, ruleId);
+        ruleProjectsInsert.setInt(2, projectId);
+        ruleProjectsInsert.executeQuery();
 
 
-//        System.out.println("\nДобавление " + artistName);
-//        projectsInsert.setString(1, artistName);
-//        projectsInsert.executeUpdate();
-//        printArtistTable(statement.executeQuery("SELECT * FROM ARTIST"));
+        var roleName = DBHelper.generateName();
+        var roleInsert = connection.prepareStatement(
+                "INSERT INTO  projects_systems.public.\"Role\" (name, description) VALUES (?, ?)"
+        );
+        roleInsert.setString(1, roleName);
+        roleInsert.setString(2, DBHelper.generateDescription());
+        roleInsert.executeQuery();
+        var roleId = dbHelper.selectRoleId(roleName);
 
-//        System.out.println("\nОбновление " + artistName + "->" + artistNextName);
-//        preparedUpdateStatement.setString(1, artistNextName);
-//        preparedUpdateStatement.setString(2, artistName);
-//        preparedUpdateStatement.executeUpdate();
-//        printArtistTable(statement.executeQuery("SELECT * FROM ARTIST"));
-//
-//        System.out.println("\nУдаление " + artistNextName);
-//        preparedDeleteStatement.setString(1, artistNextName);
-//        preparedDeleteStatement.executeUpdate();
-//        printArtistTable(statement.executeQuery("SELECT * FROM ARTIST"));
+        var employeeTeamInsert = connection.prepareStatement(
+                "INSERT INTO  projects_systems.public.\"Employee_Team\" (employee_id, team_id, role_id, start_date) VALUES (?, ?, ?, '2010/1/1')"
+        );
+        employeeTeamInsert.setInt(1, employeeId);
+        employeeTeamInsert.setInt(2, teamId);
+        employeeTeamInsert.setInt(3, roleId);
+        employeeTeamInsert.executeQuery();
+
+        var skillName = DBHelper.generateName();
+        var skillInsert = connection.prepareStatement(
+                "INSERT INTO  projects_systems.public.\"Skill\" (name, description) VALUES (?, ?)"
+        );
+        skillInsert.setString(1, skillName);
+        skillInsert.setString(2, DBHelper.generateDescription());
+        skillInsert.executeQuery();
+        var skillId =  dbHelper.selectSkillId(skillName);
+
+        var employeeSkillInsert = connection.prepareStatement(
+                "INSERT INTO  projects_systems.public.\"Employee_Skill\" (employee_id, skill_id, level) VALUES (?, ?, ?)"
+        );
+        employeeSkillInsert.setInt(1, employeeId);
+        employeeSkillInsert.setInt(2, skillId);
+        employeeSkillInsert.executeQuery();
+
+        var historyInsert = connection.prepareStatement(
+                "INSERT INTO  projects_systems.public.\"E_History\" (employee_id, description, change_date) VALUES (?, ?, '2022.1.2')"
+        );
+        historyInsert.setInt(1, employeeId);
+        historyInsert.setString(2, DBHelper.generateDescription());
+        historyInsert.executeQuery();
     }
 
     private static void printFirstQueryResult(ResultSet resultSet) {
--- lab-postgre-sql/src/helpers/DBHelper.java
+++ lab-postgre-sql/src/helpers/DBHelper.java
@@ -1,6 +1,15 @@
 package helpers;
 
+import java.sql.*;
+
 public class DBHelper {
+
+    private final Connection connection;
+
+    public DBHelper(Connection connection) {
+        this.connection = connection;
+    }
+
     public static String generateDescription(){
         return "Generated Description " + Math.exp(Math.random());
     }
@@ -8,4 +17,62 @@ public class DBHelper {
     public static String generateName(){
         return "Generated Name " + Math.exp(Math.random());
     }
+
+    public int selectProjectId(String description) {
+        try {
+            var statement = connection.prepareStatement("SELECT * FROM projects_systems.public.\"Projects\" WHERE description == ?");
+            statement.setString(1, description);
+            var result = statement.executeQuery();
+            return result.getInt(1);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return -1;
+        }
+    }
+
+    public int selectTeamId(String name) {
+        return selectIdByName(name, "SELECT * FROM projects_systems.public.\"Team\" WHERE name == ?");
+    }
+
+    public int selectPositionId(String name) {
+        return selectIdByName(name, "SELECT * FROM projects_systems.public.\"Position\" WHERE name == ?");
+    }
+
+    public int selectEmployeeId(String name) {
+        return selectIdByName(name, "SELECT * FROM projects_systems.public.\"Employee\" WHERE name == ?");
+    }
+
+    public int selectCategoriesId(String name) {
+        return selectIdByName(name, "SELECT * FROM projects_systems.public.\"P_Categories\" WHERE name == ?");
+    }
+
+    public int selectRuleId(String name) {
+        return selectIdByName(name, "SELECT * FROM projects_systems.public.\"Rule\" WHERE name == ?");
+    }
+
+    public int selectRoleId(String name) {
+        return selectIdByName(name, "SELECT * FROM projects_systems.public.\"Role\" WHERE name == ?");
+    }
+
+    public int selectSkillId(String name) {
+        return selectIdByName(name, "SELECT * FROM projects_systems.public.\"Skill\" WHERE name == ?");
+    }
+
+    public int selectIdByName(String name, String query) {
+        try {
+            var statement = connection.prepareStatement(query);
+            statement.setString(1, name);
+            var result = statement.executeQuery();
+            return result.getInt(1);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return -1;
+        }
+    }
+
+    public int selectRepositoryIdByDescription(String description) {
+        return selectIdByName(description, "SELECT * FROM projects_systems.public.\"Repository\" WHERE description == ?");
+    }
+
+
 }
