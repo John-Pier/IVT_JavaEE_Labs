0f2d5de04d06c654625ac706d63afbbbb71edb1e
John Pier <warhamer10tw@gmail.com>
2022-05-10 16:21:31 +0400

    lab-neo4j upd

--- lab-neo4j/src/main/java/Neo4jMain.java
+++ lab-neo4j/src/main/java/Neo4jMain.java
@@ -1,5 +1,9 @@
+import helpers.DBHelper;
 import org.neo4j.driver.*;
 import org.neo4j.driver.Driver;
+
+import java.sql.SQLException;
+import java.util.Random;
 import java.util.logging.Logger;
 
 public class Neo4jMain {
@@ -15,170 +19,97 @@ public class Neo4jMain {
 
         try (Session session = driver.session())
         {
-            System.out.println(session.isOpen());
-//            String greeting = session.writeTransaction( tx ->
-//            {
-//                Result result = tx.run( "CREATE (a:Greeting) " +
-//                                "SET a.message = $message " +
-//                                "RETURN a.message + ', from node ' + id(a)",
-//                        Values.parameters( "message", "message") );
-//                return result.single().get( 0 ).asString();
-//            } );
-//            System.out.println( greeting );
+            //извлечь, показать
+            insertOperations(session);
         }
     }
 
-//    private static void insertOperations(Connection connection) throws SQLException {
-//        var projectName = "Product " + Math.exp(Math.random());
-//        var projectsInsert = connection.prepareStatement(
-//                "INSERT INTO  projects_systems.projects (description) VALUES (?)"
-//        );
-//        projectsInsert.setString(1, projectName);
-//        projectsInsert.executeUpdate();
-//        var projectId = dbHelper.selectFirstProjectId();
-//
-//        if (projectId == null) {
-//            System.out.println("Error: projectId == null");
-//            return;
-//        }
-//
-//        var teamInsert = connection.prepareStatement(
-//                "INSERT INTO  projects_systems.team (name, description) VALUES (?, ?)"
-//        );
-//        var name = DBHelper.generateName();
-//        teamInsert.setString(1, name);
-//        teamInsert.setString(2, DBHelper.generateDescription());
-//        teamInsert.executeUpdate();
-//        var teamId = dbHelper.selectTeamId(name);
-//
-//        var teamProjectsInsert = connection.prepareStatement(
-//                "INSERT INTO  projects_systems.team_projects (project_id, team_id) VALUES (?, ?)"
-//        );
-//        teamProjectsInsert.setString(1, projectId);
-//        teamProjectsInsert.setString(2, teamId);
-//        teamProjectsInsert.executeUpdate();
-//
-//        var positionName = DBHelper.generateName();
-//        var positionInsert = connection.prepareStatement(
-//                "INSERT INTO  projects_systems.position (name, description) VALUES (?, ?)"
-//        );
-//        positionInsert.setString(1, positionName);
-//        positionInsert.setString(2, DBHelper.generateDescription());
-//        positionInsert.executeUpdate();
-//        var positionId = dbHelper.selectPositionId(positionName);
-//
-//        if (positionId == null) {
-//            System.out.println("Error: positionId == null");
-//            return;
-//        }
-//
-//        var employeeName = DBHelper.generateName();
-//        var employeeInsert = connection.prepareStatement(
-//                "INSERT INTO  projects_systems.employee (name, start_date, position_id, data) VALUES (?, '2001.12.11', ?, 'No data')"
-//        );
-//        employeeInsert.setString(1, employeeName);
-//        employeeInsert.setString(2, positionId);
-//        employeeInsert.executeUpdate();
-//        var employeeId = dbHelper.selectEmployeeId(employeeName);
-//
-//        var userInsert = connection.prepareStatement(
-//                "INSERT INTO  projects_systems.user (name, employee_id, credentials, data) VALUES (?, ?, '-?-', 'No data')"
-//        );
-//        userInsert.setString(1, DBHelper.generateName());
-//        userInsert.setString(2, employeeId);
-//        userInsert.executeUpdate();
-//
-//        var categoriesName = DBHelper.generateName();
-//        var categoriesInsert = connection.prepareStatement(
-//                "INSERT INTO  projects_systems.p_categories (name, description) VALUES (?, ?)"
-//        );
-//        categoriesInsert.setString(1, categoriesName);
-//        categoriesInsert.setString(2, DBHelper.generateDescription());
-//        categoriesInsert.executeUpdate();
-//        var categoriesId = dbHelper.selectCategoriesId(categoriesName);
-//
-//        var categoriesProjectsInsert = connection.prepareStatement(
-//                "INSERT INTO  projects_systems.p_categories_projects (project_id, p_category_id) VALUES (?, ?)"
-//        );
-//        categoriesProjectsInsert.setString(1, projectId);
-//        categoriesProjectsInsert.setString(2, categoriesId);
-//        categoriesProjectsInsert.executeUpdate();
-//
-//        var repositoryInsert = connection.prepareStatement(
-//                "INSERT INTO  projects_systems.repository (link, description) VALUES ('-NO LINK-', ?)"
-//        );
-//        repositoryInsert.setString(1, DBHelper.generateDescription());
-//        repositoryInsert.executeUpdate();
-//        var repositoryId = dbHelper.selectFirstRepositoryId();
-//
-//        if (repositoryId == null) {
-//            System.out.println("Error: repositoryId == null");
-//            return;
-//        }
-//
-//        var repositoryProjectsInsert = connection.prepareStatement(
-//                "INSERT INTO  projects_systems.repository_projects (project_id, repository_id) VALUES (?, ?)"
-//        );
-//        repositoryProjectsInsert.setString(1, projectId);
-//        repositoryProjectsInsert.setString(2, repositoryId);
-//        repositoryProjectsInsert.executeUpdate();
-//
-//        var ruleName = DBHelper.generateName();
-//        var ruleInsert = connection.prepareStatement(
-//                "INSERT INTO  projects_systems.rule (name, description) VALUES (?, ?)"
-//        );
-//        ruleInsert.setString(1, ruleName);
-//        ruleInsert.setString(2, DBHelper.generateDescription());
-//        ruleInsert.executeUpdate();
-//        var ruleId = dbHelper.selectRuleId(ruleName);
-//
-//        var ruleProjectsInsert = connection.prepareStatement(
-//                "INSERT INTO  projects_systems.rule_projects (rule_id, project_id) VALUES (?, ?)"
-//        );
-//        ruleProjectsInsert.setString(1, ruleId);
-//        ruleProjectsInsert.setString(2, projectId);
-//        ruleProjectsInsert.executeUpdate();
-//
-//
-//        var roleName = DBHelper.generateName();
-//        var roleInsert = connection.prepareStatement(
-//                "INSERT INTO  projects_systems.role (name, description) VALUES (?, ?)"
-//        );
-//        roleInsert.setString(1, roleName);
-//        roleInsert.setString(2, DBHelper.generateDescription());
-//        roleInsert.executeUpdate();
-//        var roleId = dbHelper.selectRoleId(roleName);
-//
-//        var employeeTeamInsert = connection.prepareStatement(
-//                "INSERT INTO  projects_systems.employee_team (employee_id, team_id, role_id, start_date) VALUES (?, ?, ?, '2010/1/1')"
-//        );
-//        employeeTeamInsert.setString(1, employeeId);
-//        employeeTeamInsert.setString(2, teamId);
-//        employeeTeamInsert.setString(3, roleId);
-//        employeeTeamInsert.executeUpdate();
-//
-//        var skillName = DBHelper.generateName();
-//        var skillInsert = connection.prepareStatement(
-//                "INSERT INTO  projects_systems.skill (name, description) VALUES (?, ?)"
-//        );
-//        skillInsert.setString(1, skillName);
-//        skillInsert.setString(2, DBHelper.generateDescription());
-//        skillInsert.executeUpdate();
-//        var skillId = dbHelper.selectSkillId(skillName);
-//
-//        var employeeSkillInsert = connection.prepareStatement(
-//                "INSERT INTO  projects_systems.employee_skill (employee_id, skill_id, level) VALUES (?, ?, ?)"
-//        );
-//        employeeSkillInsert.setString(1, employeeId);
-//        employeeSkillInsert.setString(2, skillId);
-//        employeeSkillInsert.setString(3, DBHelper.generateDescription());
-//        employeeSkillInsert.executeUpdate();
-//
-//        var historyInsert = connection.prepareStatement(
-//                "INSERT INTO  projects_systems.e_History (employee_id, description, change_date) VALUES (?, ?, '2022.1.2')"
-//        );
-//        historyInsert.setString(1, employeeId);
-//        historyInsert.setString(2, DBHelper.generateDescription());
-//        historyInsert.executeUpdate();
-//    }
+    private static void selectProjectsWithTeams(Result result) {
+
+    }
+
+    private static void printProjects(Result result) {
+
+    }
+
+    private static void insertOperations(Session session) throws SQLException {
+        Random random = new Random();
+        var projectId = random.nextInt(90000000);
+        var repositoryId = random.nextInt(90000000);
+        var categoryId = random.nextInt(90000000);
+        var ruleId = random.nextInt(90000000);
+        var teamId = random.nextInt(90000000);
+        var projectName = "Product " + Math.exp(Math.random());
+
+        session.writeTransaction(transaction -> {
+            transaction.run(
+                    "create (p:Project {description: $description, projectId: $projectId}) " +
+                            "create (r:Repository {description: $description4, url: $url, id: $repositoryId}) " +
+                            "create (c:Category {description: $description1, name: $name, id: $categoryId}) " +
+                            "create (t:Team {description: $description2, name: $name1, id: $teamId}) " +
+                            "create (ru:Rule {description: $description3, name: $name2, id: $ruleId}) ",
+                    Values.parameters(
+                            "description", projectName,
+                            "projectId", projectId,
+                            "ruleId", ruleId,
+                            "repositoryId", repositoryId,
+                            "categoryId", categoryId,
+                            "teamId", teamId,
+                            "url", "test-1",
+                            "description1", DBHelper.generateDescription(),
+                            "description2", DBHelper.generateDescription(),
+                            "description3", DBHelper.generateDescription(),
+                            "description4", DBHelper.generateDescription(),
+                            "name", DBHelper.generateName(),
+                            "name1", DBHelper.generateName(),
+                            "name2", DBHelper.generateName()
+                    )
+            );
+            transaction.run(
+                    """
+                            MATCH (p:Project) where p.projectId = $projectId\s
+                            MATCH (r:Repository) where r.id = $repositoryId\s
+                            MERGE (p)-[pr:PROJECT_HAVE_REPO]-(r)\s
+                            \s""",
+                    Values.parameters(
+                            "projectId", projectId,
+                            "repositoryId", repositoryId
+                    )
+            );
+            transaction.run(
+                    """
+                            MATCH (p:Project) where p.projectId = $projectId\s
+                            MATCH (c:Category) where c.id = $categoryId\s
+                            MERGE (p)-[pr:PROJECT_HAVE_CATEGORY]-(c)\s
+                            \s""",
+                    Values.parameters(
+                            "projectId", projectId,
+                            "categoryId", categoryId
+                    )
+            );
+            transaction.run(
+                    """
+                            MATCH (p:Project) where p.projectId = $projectId\s
+                            MATCH (t:Team) where t.id = $teamId\s
+                            MERGE (p)-[pr:PROJECT_HAVE_TEAM]-(t)\s
+                            \s""",
+                    Values.parameters(
+                            "projectId", projectId,
+                            "teamId", teamId
+                    )
+            );
+            transaction.run(
+                    """
+                            MATCH (p:Project) where p.projectId = $projectId\s
+                            MATCH (ru:Rule) where ru.id = $ruleId\s
+                            MERGE (p)-[pr:PROJECT_HAVE_RULE]-(ru)\s
+                            \s""",
+                    Values.parameters(
+                            "projectId", projectId,
+                            "ruleId", ruleId
+                    )
+            );
+            return null;
+        });
+    }
 }
---
+++ lab-neo4j/src/main/java/helpers/DBHelper.java
@@ -0,0 +1,86 @@
+package helpers;
+
+import java.sql.Connection;
+import java.util.Random;
+
+public class DBHelper {
+
+//    private final Connection connection;
+
+//    public DBHelper(Connection connection) {
+//        this.connection = connection;
+//    }
+private static final Random random = new Random();
+    public static String generateDescription(){
+        return "Generated Description " + random.nextInt(10000);
+    }
+
+    public static String generateName(){
+        return "Generated Name " + random.nextInt(10000);
+    }
+
+//    public String selectTeamId(String name) {
+//        return selectIdByParam(name, "SELECT * FROM projects_systems.team WHERE name = ?");
+//    }
+//
+//    public String selectPositionId(String name) {
+//        return selectIdByParam(name, "SELECT * FROM projects_systems.position WHERE name = ?");
+//    }
+//
+//    public String selectEmployeeId(String name) {
+//        return selectIdByParam(name, "SELECT * FROM projects_systems.employee WHERE name = ?");
+//    }
+//
+//    public String selectCategoriesId(String name) {
+//        return selectIdByParam(name, "SELECT * FROM projects_systems.p_categories WHERE name = ?");
+//    }
+//
+//    public String selectRuleId(String name) {
+//        return selectIdByParam(name, "SELECT * FROM projects_systems.rule WHERE name = ?");
+//    }
+//
+//    public String selectRoleId(String name) {
+//        return selectIdByParam(name, "SELECT * FROM projects_systems.role WHERE name = ?");
+//    }
+//
+//    public String selectSkillId(String name) {
+//        return selectIdByParam(name, "SELECT * FROM projects_systems.skill WHERE name = ?");
+//    }
+//
+//    private String selectIdByParam(String param, String query) {
+//        try {
+//            var statement = connection.prepareStatement(query);
+//            statement.setString(1, param);
+//            var result = statement.executeQuery();
+//            if (result.next()) {
+//                return result.getString(1);
+//            }
+//            return null;
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            return null;
+//        }
+//    }
+//
+//    public String selectFirstRepositoryId() {
+//        return selectFirstId("SELECT * FROM projects_systems.repository ORDER BY description DESC");
+//    }
+//
+//    public String selectFirstProjectId() {
+//        return selectFirstId("SELECT * FROM projects_systems.projects");
+//    }
+//
+//    private String selectFirstId(String query) {
+//        try {
+//            var statement = connection.prepareStatement(query);
+//            var result = statement.executeQuery();
+//            if (result.next()) {
+//                return result.getString(1);
+//            }
+//            return null;
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            return null;
+//        }
+//    }
+}
