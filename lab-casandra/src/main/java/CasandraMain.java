d1792e3566313a74b1ed23f90edd021ba41413fb
John Pier <warhamer10tw@gmail.com>
2022-06-05 14:54:51 +0400

    upd

--- lab-casandra/src/main/java/CasandraMain.java
+++ lab-casandra/src/main/java/CasandraMain.java
@@ -6,13 +6,9 @@ import java.net.InetSocketAddress;
 
 /***
  * To run Docker image:
- * docker network create cassandra
- * docker run --rm --network cassandra -e CQLSH_HOST=cassandra -e CQLSH_PORT=9042 -e CQLVERSION=3.4.5 nuvo/docker-cqlsh
- * docker run --rm -d --name cassandra  -p 9042:9042 --hostname cassandra --network cassandra cassandra
- * docker network rm cassandra
- *  -e CASSANDRA_PASSWORD=cassandra
  *  docker run --name casandra-inst  -p 9042:9042 -p 7000:7000 -p 9160:9160 -p 7199:7199 -d cassandra
- * cassandra
+ * login -> cassandra
+ * to set password -> -e CASSANDRA_PASSWORD=cassandra
  */
 
 public class CasandraMain {
